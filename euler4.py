329e3c8c254f56986580148048b34bd8bd26a5f9
Eric White <led.ecodyswhite@gmail.com>
2022-12-09 22:49:34 -0800

    bleh nested loops

--- euler4.py
+++ euler4.py
@@ -2,27 +2,18 @@
 
 Find the largest palindrome made from the product of two 3-digit numbers.'''
 
-#largest sum of two three-digit numbers: 998001
-
-target = 998001
 palinList = []
-palinFactor = []
 
 def isPalin(n):
     if str(n) == str(n)[::-1]:
         return True
 
-def isPalinFactorial(n):
+def isPalinFactorial(list):
 
-    for x in range(10000, target):
-        if isPalin(x):
-            palinList.append(x)
+    for x in range(100, 999):
+        for y in range(100, 999):
+            if isPalin(x * y):
+                list.append(x * y)
+    print(max(list))
 
-    for x in range(10000, target):
-        if n % x == 0 and isPalin(x):
-            palinFactor.append(x)
-            print (palinFactor)
-
-isPalinFactorial(target)
-
-#There's an error in my logic and I'm not sure where.
+isPalinFactorial(palinList)
